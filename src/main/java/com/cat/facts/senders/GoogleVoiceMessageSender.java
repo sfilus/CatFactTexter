6cee22954dedf4d372386c199be72e5593863ce5
Stephen Filus <funkmasterfilus@gmail.com>
2015-01-21 08:23:43 -0500

    Add message sender

---
+++ src/main/java/com/cat/facts/senders/GoogleVoiceMessageSender.java
@@ -0,0 +1,48 @@
+package com.cat.facts.senders;
+
+import com.techventus.server.voice.Voice;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Send messages using Google Voice.
+ * @author Steve
+ */
+public class GoogleVoiceMessageSender implements MessageSender{
+
+    private final Voice voice;
+    
+    /**
+     * Constructor initializes Google Voice instance.
+     * @param username
+     * @param password
+     * @throws java.io.IOException
+     */
+    public GoogleVoiceMessageSender(String username, String password) throws IOException
+    {
+        voice = new Voice(username, password);
+    }
+    
+    /**
+     * Text a single recipient a message.
+     * @param recipient phone number
+     * @param message message to text
+     */
+    @Override
+    public void send(String recipient, String message) throws IOException{
+        voice.sendSMS(recipient, message);
+    }
+
+    /**
+     * Text multiple recipients the same message.
+     * @param recipients
+     * @param message 
+     */
+    @Override
+    public void send(List<String> recipients, String message) throws IOException{
+        for(String recipient : recipients) {
+            voice.sendSMS(recipient, message);
+        }
+    }
+    
+}
---
+++ src/main/java/com/cat/facts/senders/MessageSender.java
@@ -0,0 +1,27 @@
+package com.cat.facts.senders;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Interface defining classes that are capable of sending messages.
+ * @author Steve
+ */
+public interface MessageSender {
+    
+    /**
+     * Send a message to a single recipient.
+     * @param recipient string
+     * @param message string
+     * @throws java.io.IOException
+     */
+    public void send(String recipient, String message) throws IOException;
+    
+    /**
+     * Send the same message to multiple recipients.
+     * @param recipients
+     * @param message 
+     * @throws java.io.IOException 
+     */
+    public void send(List<String> recipients, String message) throws IOException;
+}
